2df11c0c80f0671e5a27671a1f9a7bced9e44043
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-05-29 12:43:49 +0900

    Set border

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -528,8 +528,8 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
-client.connect_signal("focus", function(c) c.border_color = "#00cc00" end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.connect_signal("focus", function(c) c.border_color = "#3333ff" end)
+client.connect_signal("unfocus", function(c) c.border_color = "#999999" end)
 -- }}}
 
 gears.wallpaper.set("#666666")
