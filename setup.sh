6a3702674780db1710e12620ecb0c8feee95ff04
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-06-07 00:43:02 +0900

    Fix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-mkdir -p 
-ln -sf 
+set -e
+
+targets=$(find . | grep -v /.git[/$] | grep -v README.md | grep -v setup.sh)
+for t in $targets
+do
+	destpath=$(pwd)/$t
+	if [[ -d $t ]]
+	then
+		(
+			cd ~
+			mkdir -p "$t"
+		)
+	elif [[ -f $t ]]
+	then
+		destPath=$(pwd)/$t
+		(
+			cd ~/$(dirname "$t")
+			cmd="ln -sf $destPath"
+			echo $cmd
+			$cmd
+		)
+	fi
+done
