4bfc7154379df033a3a6b509032244a8ac0ab490
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-01-06 13:51:54 +0900

    Add

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -3,7 +3,7 @@ c = get_config()
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = [
     '%autoreload 3',
-    'import re, os, math, datetime, asyncio',
+    'import re, os, math, datetime, asyncio, httpx, json, sys'
 ]
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.colors = 'NoColor'
