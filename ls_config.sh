a410e73bb28b5ee9309a44c51fdd3867e54e7240
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-06-01 09:50:46 +0900

    Add

--- ls_config.sh
+++ ls_config.sh
@@ -4,7 +4,7 @@ set -eu
 
 git_ignores=$(cat .gitignore | sed -E 's/^\///' | sed -E 's/\/$//')
 
-find_cmd='find . -not -path . -and -not -path "./.git*" -and -not -path ./.gitignore -and -not -path ./README.md -and -not -path "./mk-tools*" -and -not -path ./.envrc -and -not -path "./*_config.sh"'
+find_cmd='find . -not -path . -and -not -path "./.git" -and -not -path "./.git/*" -and -not -path ./.gitignore -and -not -path ./README.md -and -not -path "./mk-tools*" -and -not -path ./.envrc -and -not -path "./*_config.sh"'
 for path in $git_ignores
 do
     find_cmd="$find_cmd -and -not -path \"./$path*\""
